a44906902c0c235e47d4fee7e4a3fd1d0b8ffd19
luizinhoguhh14-cmyk <luizinhoguhh14@gmail.com>
2026-09-12 03:20:45 +0000

    fix: estabiliza sdk em 34 para evitar integer overflow no flutter

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,12 +20,12 @@ subprojects {
             try {
                 // Forca o SDK 36 no AGP 8+
                 val method = androidExt.javaClass.getMethod("setCompileSdk", java.lang.Integer::class.java)
-                method.invoke(androidExt, 36)
+                method.invoke(androidExt, 34)
             } catch (e: Exception) {
                 try {
                     // Forca o SDK 36 em versoes anteriores
                     val method2 = androidExt.javaClass.getMethod("setCompileSdkVersion", Int::class.javaPrimitiveType)
-                    method2.invoke(androidExt, 36)
+                    method2.invoke(androidExt, 34)
                 } catch (e2: Exception) {}
             }
         }
