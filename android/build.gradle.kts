89b8750dfb8346b4372a178597e980e8903b7572
luizinhoguhh14-cmyk <luizinhoguhh14@gmail.com>
2026-09-13 20:46:02 +0000

    fix: recria build.gradle.kts raiz com sintaxe correta e sdk 36

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -14,10 +14,15 @@ subprojects {
 }
 
 subprojects {
-
-subprojects {
+    project.evaluationDependsOn(":app")
+    
+    // Força o compileSdk 36 nos plugins para evitar falhas do file_picker e audioplayers
     plugins.withId("com.android.library") {
         val androidExt = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
         androidExt?.compileSdkVersion(36)
     }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
